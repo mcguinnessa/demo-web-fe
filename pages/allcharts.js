7bce9b4d326db8e70e98d67534c2f4b1f89a5dc2
Ubuntu <ubuntu@ip-172-31-45-151.eu-west-2.compute.internal>
2023-06-23 09:06:03 +0000

    remove port

--- pages/allcharts.js
+++ pages/allcharts.js
@@ -14,7 +14,8 @@ export async function getStaticProps() {
   const wrapper_port = process.env.WRAPPER_PORT
 //  const wrapper_metric = process.env.WRAPPER_METRIC
 	//const wrapper_url = "http://" + wrapper_host + ":" + wrapper_port + "/" 
-  const wrapper_url = wrapper_host + ":" + wrapper_port + "/" 
+//  const wrapper_url = wrapper_host + ":" + wrapper_port + "/" 
+  const wrapper_url = wrapper_host + "/" 
   console.log("Requesting:" + wrapper_url)
 
   //const res = await fetch('https://api.github.com/repos/developit/preact')
