34160bc9fd52d87f62f288d61a54f72abdbc1257
Ubuntu <ubuntu@ip-172-31-8-129.eu-west-2.compute.internal>
2023-06-26 15:38:33 +0000

    add permemant LB

--- terraform/main.tf
+++ terraform/main.tf
@@ -123,12 +123,13 @@ resource "aws_ecs_service" "webfe_service" {
 
 
   load_balancer {
-    target_group_arn = "${aws_lb_target_group.webfe_target_group.arn}" # Referencing our target group
+    #target_group_arn = "${aws_lb_target_group.webfe_target_group.arn}" # Referencing our target group
+    target_group_arn = "arn:aws:elasticloadbalancing:eu-west-2:182028175464:targetgroup/web-target-group/36d0fe06e8a225b1"
     container_name   = "${aws_ecs_task_definition.webfe_task.family}"
     container_port   = 3000 # Specifying the container port
   }
 
-  depends_on = [aws_lb_listener.listener]
+#  depends_on = [aws_lb_listener.listener]
 
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
@@ -148,7 +149,8 @@ resource "aws_security_group" "service_security_group" {
     to_port   = 0
     protocol  = "-1"
     # Only allowing traffic in from the load balancer security group
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+    #security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+    security_groups = ["sg-039063a37c674e76b", "sg-05aca12900ed1f068"]
   }
 
   egress {
@@ -180,56 +182,56 @@ resource "aws_default_subnet" "default_subnet_c" {
 #
 # The Load balancer
 #
-resource "aws_alb" "application_load_balancer" {
-  name               = "webfe-lb" # Naming our load balancer
-  load_balancer_type = "application"
-  subnets = [ # Referencing the default subnets
-    "${aws_default_subnet.default_subnet_a.id}",
-    "${aws_default_subnet.default_subnet_b.id}",
-    "${aws_default_subnet.default_subnet_c.id}"
-  ]
-  # Referencing the security group
-  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-}
-
-# Creating a security group for the load balancer:
-resource "aws_security_group" "load_balancer_security_group" {
-  ingress {
-    from_port   = 80 # Allowing traffic in from port 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
-
-  egress {
-    from_port   = 0 # Allowing any incoming port
-    to_port     = 0 # Allowing any outgoing port
-    protocol    = "-1" # Allowing any outgoing protocol 
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  }
-}
-
-resource "aws_lb_target_group" "webfe_target_group" {
-  name        = "webfe-target-group"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
-  health_check {
-    matcher = "200,301,302,308"
-    path = "/"
-  }
-}
-
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.webfe_target_group.arn}" # Referencing our target group
-  }
-}
+#resource "aws_alb" "application_load_balancer" {
+#  name               = "webfe-lb" # Naming our load balancer
+#  load_balancer_type = "application"
+#  subnets = [ # Referencing the default subnets
+#    "${aws_default_subnet.default_subnet_a.id}",
+#    "${aws_default_subnet.default_subnet_b.id}",
+#    "${aws_default_subnet.default_subnet_c.id}"
+#  ]
+#  # Referencing the security group
+#  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+#}
+#
+## Creating a security group for the load balancer:
+#resource "aws_security_group" "load_balancer_security_group" {
+#  ingress {
+#    from_port   = 80 # Allowing traffic in from port 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+#  }
+#
+#  egress {
+#    from_port   = 0 # Allowing any incoming port
+#    to_port     = 0 # Allowing any outgoing port
+#    protocol    = "-1" # Allowing any outgoing protocol 
+#    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+#  }
+#}
+#
+#resource "aws_lb_target_group" "webfe_target_group" {
+#  name        = "webfe-target-group"
+#  port        = 80
+#  protocol    = "HTTP"
+#  target_type = "ip"
+#  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+#  health_check {
+#    matcher = "200,301,302,308"
+#    path = "/"
+#  }
+#}
+#
+#resource "aws_lb_listener" "listener" {
+#  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+#  port              = "80"
+#  protocol          = "HTTP"
+#  default_action {
+#    type             = "forward"
+#    target_group_arn = "${aws_lb_target_group.webfe_target_group.arn}" # Referencing our target group
+#  }
+#}
 
 
 
