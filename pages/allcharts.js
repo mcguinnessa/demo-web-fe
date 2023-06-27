671aad8f055a6fde2503603b13b7baeec936bf96
Ubuntu <ubuntu@ip-172-31-45-151.eu-west-2.compute.internal>
2023-06-27 09:42:24 +0000

    format axis

--- pages/allcharts.js
+++ pages/allcharts.js
@@ -12,8 +12,6 @@ export async function getStaticProps() {
 
   const wrapper_host = process.env.WRAPPER_HOST
   const wrapper_port = process.env.WRAPPER_PORT
-//  const wrapper_metric = process.env.WRAPPER_METRIC
-	//const wrapper_url = "http://" + wrapper_host + ":" + wrapper_port + "/" 
 
   var wrapper_url = ""
   if (typeof wrapper_port !== 'undefined' && wrapper_port !== null && wrapper_port !== ""){
@@ -22,25 +20,8 @@ export async function getStaticProps() {
      wrapper_url = wrapper_host + "/" 
   }	 
 
-  //wrapper_url = wrapper_host + "/" 
   console.log("Requesting:" + wrapper_url)
 
-  //const res = await fetch('https://api.github.com/repos/developit/preact')
-  //const response  = await fetch('ec2-3-8-157-149.eu-west-2.compute.amazonaws.com:3000/motraffic')
-  //const mo_resp  = await fetch('http://ec2-3-8-157-149.eu-west-2.compute.amazonaws.com:3000/motraffic')
-
-
-  /*This is an attempt at a promise to make it more efficient, but the backend needs streamlining before
-    it will work
-    */
-//  let mo_json;
-//  const mo_resp  = fetch(wrapper_url+"motraffic").then( mo_resp => {
-//    //const mo_json = await mo_resp.json()
-//    return mo_resp.json();
-//  }).then(mo_json => {
-//    return mo_json;
-//  })
-	
   var mo_json = {timestamps : "No Data", data: "No Data"}
   var mt_json = {timestamps : "No Data", data: "No Data"}
   var cpuusage_json = {timestamps : "No Data", data: "No Data"}
@@ -49,28 +30,22 @@ export async function getStaticProps() {
   var diskusage_json ={timestamps : "No Data", data: "No Data"}
 
   try {
-  const mo_resp  = await fetch(wrapper_url+"motraffic")
-  const mt_resp  = await fetch(wrapper_url+"mttraffic")
-  const cpuusage_resp  = await fetch(wrapper_url+"cpuusage")
-  const cputemp_resp  = await fetch(wrapper_url+"cputemp")
-  const memusage_resp  = await fetch(wrapper_url+"memusage")
-  const diskusage_resp  = await fetch(wrapper_url+"diskusage")
+    const mo_resp  = await fetch(wrapper_url+"motraffic")
+    const mt_resp  = await fetch(wrapper_url+"mttraffic")
+    const cpuusage_resp  = await fetch(wrapper_url+"cpuusage")
+    const cputemp_resp  = await fetch(wrapper_url+"cputemp")
+    const memusage_resp  = await fetch(wrapper_url+"memusage")
+    const diskusage_resp  = await fetch(wrapper_url+"diskusage")
 
-  mo_json = await mo_resp.json()
-  mt_json = await mt_resp.json()
-  cpuusage_json = await cpuusage_resp.json()
-  cputemp_json = await cputemp_resp.json()
-  memusage_json = await memusage_resp.json()
-  diskusage_json = await diskusage_resp.json()
+    mo_json = await mo_resp.json()
+    mt_json = await mt_resp.json()
+    cpuusage_json = await cpuusage_resp.json()
+    cputemp_json = await cputemp_resp.json()
+    memusage_json = await memusage_resp.json()
+    diskusage_json = await diskusage_resp.json()
   } catch (err) {
     console.log("Failed to get data:" + err);
   }
-//  console.log("IS OK")
-//  console.log(mo_resp.ok)
-//  console.log(mo_json)
-//  console.log(mt_json)
-//  console.log(cputemp_json)
-//  console.log("AFTER2")
 
   return {
     props: {
@@ -134,6 +109,14 @@ function Example({mo_json, mt_json, cpuusage_json, cputemp_json, memusage_json, 
 	//responsive means the chart is resized when the window is
         responsive: true,
         maintainAspectRatio: true,
+	scales: {
+	  x: {
+            type: 'time',
+	    time: {
+	       unit: 'hour'
+	    }
+	  }
+	}
 //        layout: {
 //          padding: {
 //            top: 5,
