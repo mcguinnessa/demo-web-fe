61e3a74fa96bc5d28b6252770c437332fb680200
Ubuntu <ubuntu@ip-172-31-4-79.eu-west-2.compute.internal>
2024-06-11 13:44:27 +0000

    change SGs to generic

--- terraform/main.tf
+++ terraform/main.tf
@@ -157,7 +157,8 @@ resource "aws_security_group" "service_security_group" {
     protocol  = "-1"
     # Only allowing traffic in from the load balancer security group
     #security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-    security_groups = ["sg-039063a37c674e76b", "sg-05aca12900ed1f068"]
+    #security_groups = ["sg-039063a37c674e76b", "sg-05aca12900ed1f068"]
+    security_groups = ["ADM-Demo-UI-LB-SG"]
   }
 
   egress {
