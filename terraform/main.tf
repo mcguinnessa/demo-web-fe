b29e7667253da01f84dbfb07eef0ff6762a54f41
Ubuntu <ubuntu@ip-172-31-39-201.eu-west-2.compute.internal>
2024-06-13 10:00:57 +0000

    add in security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -160,7 +160,7 @@ resource "aws_security_group" "service_security_group" {
     # Only allowing traffic in from the load balancer security group
     #security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
     #security_groups = ["sg-039063a37c674e76b", "sg-05aca12900ed1f068"]
-    security_groups = ["sg-08250d7710c495a8d"]
+    security_groups = ["sg-0bb3edbeed5618502"]
   }
 
   egress {
