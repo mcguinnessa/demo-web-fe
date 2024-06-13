153351565325dcf0caeb6cc0e82813f6e9a111f2
Ubuntu <ubuntu@ip-172-31-39-201.eu-west-2.compute.internal>
2024-06-13 15:06:09 +0000

    Change MO fill

--- pages/allcharts.js
+++ pages/allcharts.js
@@ -82,7 +82,6 @@ function Example({mo_json, mt_json, cpuusage_json, cputemp_json, memusage_json, 
           label: "MO",
           borderColor: "#2d0ce8",
           backgroundColor: "#6a53ed",
-	#fill: true,
           fill: false,
         }
         ]
