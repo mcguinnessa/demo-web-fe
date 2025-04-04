7b192205ffad22615e0383d3350d051617588c3a
Ubuntu <ubuntu@ip-172-31-35-162.eu-west-2.compute.internal>
2025-04-04 17:35:53 +0000

    Update LB Target Group

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,10 +84,10 @@ resource "aws_ecs_task_definition" "webfe_task" {
   execution_role_arn       = "arn:aws:iam::637423404396:role/AlexECSTaskExecutionRole"
 }
 
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-}
+#resource "aws_iam_role" "ecsTaskExecutionRole" {
+#  name               = "ecsTaskExecutionRole"
+#  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+#}
 
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
