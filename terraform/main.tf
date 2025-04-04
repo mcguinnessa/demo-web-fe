575ce1813ca8795011091f527bb8ac6bc574299f
Ubuntu <ubuntu@ip-172-31-35-162.eu-west-2.compute.internal>
2025-04-04 17:39:39 +0000

    Update LB Target Group

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,10 +100,10 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+#resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+#  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#}
 
 resource "aws_ecs_service" "webfe_service" {
   name            = "webfe-service"                             # Naming our first service
